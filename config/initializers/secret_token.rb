92d42565bc91c0e0d48149edf00e5a631cd54223
Travis <travisR004@gmail.com>
2014-04-08 23:23:44 -0700

    Initialized Bridge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bridge::Application.config.secret_key_base = '69dceec209c8949ed4bae130dc687bba01401e0ab2cb24784f338cf7bc7dfb7bd0bbb92305bb0426215cd8e465aa61dfb68ee7493cc9cd2129fdceaee5df19b3'
