83c8099dda52c65a28c2572226c8294355b7e243
Travis <travisR004@gmail.com>
2014-05-11 15:27:58 -0700

    fixed bug for realz

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -18,7 +18,7 @@ class Api::ProjectsController < ApplicationController
   def create
     @project = current_user.projects.new(project_params)
     if @project.save
-      if params[:image][:photo]
+      if params[:image]
         params[:image][:photo].each do |photo|
           image = Image.new(project_id: @project.id)
           image.photo = photo
